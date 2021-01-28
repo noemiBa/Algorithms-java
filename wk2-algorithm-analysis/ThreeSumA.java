820dae39d2375db2e9b0f342bb803507e39e7451
noemiBa <77796348+noemiBa@users.noreply.github.com>
2021-01-28 18:48:44 +0000

    Added StdOut.println statement line 33
    The class now prints the correct triples.

--- wk2-algorithm-analysis/ThreeSumA.java
+++ wk2-algorithm-analysis/ThreeSumA.java
@@ -30,6 +30,7 @@ public class ThreeSumA {
             for (int j = i+1; j < n; j++) {
                 for (int k = j+1; k < n; k++) {
                     if (a[i] + a[j] + a[k] == 0) {
+                        StdOut.println("Correct triple: " + a[i] + ", " + a[j] + ", " + a[k]);
                         count++;
                     }
                 }
