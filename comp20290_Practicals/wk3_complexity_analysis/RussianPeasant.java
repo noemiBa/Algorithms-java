cee4355ecc819cbcffc53e6abf0fd9272f0234b0
noemiBa <banalnoemi@gmail.com>
2021-02-06 18:15:57 +0000

    Added Russian Peasant algorithm implementation
    as well as the analysis of its time complexity

--- comp20290_Practicals/wk3_complexity_analysis/RussianPeasant.java
+++ comp20290_Practicals/wk3_complexity_analysis/RussianPeasant.java
@@ -1,10 +1,52 @@
-package wk3_complexity_analysis;
+import java.math.BigInteger;
 
+/* A class implementing the Russian Peasant algorithm for multiplication
+ * 
+ * @Author: NoemiBa 
+ */
 public class RussianPeasant {
 
+	private BigInteger number1; 
+	private BigInteger number2; 
+
+	public RussianPeasant(BigInteger number1, BigInteger number2) {
+		this.number1 = number1; 
+		this.number2 = number2; 
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		RussianPeasant rp = new RussianPeasant(new BigInteger("1"), new BigInteger("10"));
+		Stopwatch sw = new Stopwatch(); 
+		BigInteger result = rp.multiply(); 
+		System.out.println(sw.elapsedTime());
+		System.out.println(result);
 
 	}
 
+	/* This method multiplies two integers using the Russian Peasant algorithm
+	 * 
+	 * @Return the result of the multiplication.
+	 */
+	public BigInteger multiply() {
+		BigInteger result = BigInteger.ZERO; 
+
+		while (number2.signum() == 1) { //while the second integer is still returns a positive sign...
+			
+			if (isOdd(number2)) { //if said integer is odd...
+				result = result.add(number1); //add integer1 to the result
+			}
+			number2 = number2.divide(BigInteger.TWO); 
+			number1 = number1.multiply(BigInteger.TWO); 
+		}
+
+		return result; 
+	}
+	
+	public boolean isOdd(BigInteger number) {
+	    if(!number.mod(new BigInteger("2")).equals(BigInteger.ZERO)) {
+	        return true;
+	    }
+	    return false;
+	}
 }
+
--- comp20290_Practicals/wk3_complexity_analysis/Stopwatch.java
+++ comp20290_Practicals/wk3_complexity_analysis/Stopwatch.java
@@ -1,17 +1,3 @@
-package wk2;
-/******************************************************************************
- *  Compilation:  javac Stopwatch.java
- *  Execution:    java Stopwatch n
- *  Dependencies: none
- *
- *  A utility class to measure the running time (wall clock) of a program.
- *
- *  % java8 Stopwatch 100000000
- *  6.666667e+11  0.5820 seconds
- *  6.666667e+11  8.4530 seconds
- *
- ******************************************************************************/
-
 
 /**
  *  The {@code Stopwatch} data type is for measuring
@@ -72,7 +58,6 @@ public class Stopwatch {
             sum1 += Math.sqrt(i);
         }
         double time1 = timer1.elapsedTime();
-        StdOut.printf("%e (%.2f seconds)\n", sum1, time1);
 
         // sum of square roots of integers from 1 to n using Math.pow(x, 0.5).
         Stopwatch timer2 = new Stopwatch();
@@ -81,7 +66,7 @@ public class Stopwatch {
             sum2 += Math.pow(i, 0.5);
         }
         double time2 = timer2.elapsedTime();
-        StdOut.printf("%e (%.2f seconds)\n", sum2, time2);
+        System.out.printf("%e (%.2f seconds)\n", sum2, time2);
     }
 } 
 
